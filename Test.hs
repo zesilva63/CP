256307e8fac1a6e9361d342f39f7b8e60a3f3be1
Ricardo Pereira <ricardo.agp.914@gmail.com>
2016-03-16 19:34:09 +0000

    Tarefa 1 quase completa

--- Test.hs
+++ Test.hs
@@ -17,8 +17,8 @@
 -- Curso: Mestrado Integrado em Engenharia Informática 
 --
 -- Aluno 2
--- Número: Ricardo António Gonçalves Pereira
--- Nome: A
+-- Número: A74185
+-- Nome: Ricardo António Gonçalves Pereira
 -- Curso: Mestrado Integrado em Engenharia Informática
 --
 
@@ -39,9 +39,57 @@ g1 = Graph {nodes = fromList [1],
             edges = fromList [Edge 1 1]
            }
 
+g2 :: Graph Int 
+g2 = Graph {nodes = fromList [1,2,3,4], 
+             edges = fromList [Edge 1 2,Edge 1 3,Edge 2 4,Edge 4 1]
+            }          
+
+g3F :: Graph Int 
+g3F = Graph {nodes = fromList [1,3,4,5], 
+            edges = fromList [Edge 1 2, Edge 2 3, Edge 3 4, Edge 4 1]
+            }
+
+g4 :: Graph Int 
+g4 = Graph {nodes = fromList [1,2,3,4,5], 
+            edges = fromList [Edge 1 2, Edge 2 3, Edge 3 4, Edge 4 5]
+            }
+
+g4F :: Graph Int 
+g4F = Graph {nodes = fromList [1,2,3,5], 
+             edges = fromList [Edge 1 2, Edge 2 3, Edge 3 4, Edge 4 5]
+            }
+
+g4x2 :: Graph Int
+g4x2 = Graph {nodes = fromList [1,2,3,4,5], 
+              edges = fromList [Edge 1 2,Edge 1 3, Edge 2 3, Edge 2 4,Edge 3 4 ,Edge 4 1 ,Edge 4 5]
+             } 
+
+g5 :: Graph Int 
+g5 = Graph {nodes = fromList [1,2,3,4,5], 
+            edges = fromList [Edge 1 2, Edge 2 3, Edge 3 4, Edge 4 5, Edge 5 1]
+            }                        
+
+g5T :: Graph Int 
+g5T = Graph {nodes = fromList [1,2,3,4,5], 
+            edges = fromList [Edge 2 1, Edge 3 2, Edge 4 3, Edge 5 4, Edge 1 5]
+            }                        
+
+g6 :: Graph Int 
+g6 = Graph {nodes = fromList [1,2,3,4,5], 
+            edges = fromList [Edge 1 2, Edge 1 3, Edge 1 4]
+            }                 
+
+emptyG :: Graph Int
+emptyG = Graph {nodes = fromList[], edges = fromList[]}
+
+
+e1 :: Edge Int 
+e1 = Edge 1 2
+
 -- Um exemplo de um teste unitário.
 test_adj :: Test
 test_adj = adj g1 1 ~?= fromList [Edge 1 1]
+
 
 --
 -- Tarefa 1
@@ -50,8 +98,98 @@ test_adj = adj g1 1 ~?= fromList [Edge 1 1]
 -- tentando obter o máximo de cobertura de expressões, condições, etc.
 --
 
-           
-main = runTestTT $ TestList [test_adj]
+-- Funçao swap
+test_swap :: Test 
+test_swap = swap e1 ~?= Edge 2 1  
+
+-- Função empty 
+test_empty :: Test
+test_empty = Graph.empty ~?= emptyG
+
+-- Função isEmpty
+test_isEmpty1 :: Test
+test_isEmpty1 = isEmpty g1 ~?= False
+
+test_isEmpty2 :: Test
+test_isEmpty2 = isEmpty emptyG ~?= True 
+
+-- Função isValid
+test_isValid1 :: Test
+test_isValid1 = isValid g2 ~?= True
+
+test_isValid2 :: Test
+test_isValid2 = isValid g3F ~?= False
+
+-- Funçao isDAG 
+test_isDAG1 :: Test
+test_isDAG1 = isDAG g4 ~?= True
+
+test_isDAG2 :: Test
+test_isDAG2 = isDAG g2 ~?= False
+
+test_isDAG3 :: Test 
+test_isDAG3 = isDAG g4F ~?= False 
+
+-- Funcao isForest
+test_isForest1 :: Test
+test_isForest1 = isForest g4 ~?= True
+
+test_isForest2 :: Test
+test_isForest2 = isForest g2 ~?= False
+
+-- Funcao isSubgraphOf
+test_isSubgraphOf1 :: Test
+test_isSubgraphOf1 = isSubgraphOf g4 g5 ~?= True 
+
+test_isSubgraphOf2 :: Test
+test_isSubgraphOf2 = isSubgraphOf g2 g5 ~?= False
+
+-- Funcao transpose
+
+test_transpose :: Test
+test_transpose = transpose g5 ~?= g5T
+
+-- Funcao union 
+test_union :: Test
+test_union = Graph.union g2 g4 ~?= g4x2
+
+-- Funcao bft
+test_bft1 :: Test
+test_bft1 = bft g6 (fromList[4]) ~?= Graph {nodes = fromList[4],edges = fromList[]}
+
+test_bft2 :: Test
+test_bft2 = bft g6 (fromList[1]) ~?= Graph {nodes = fromList[1,2,3,4],edges = fromList [Edge 2 1,Edge 3 1,Edge 4 1]} 
+
+--test_bft3 :: Test
+--test_bft3 = bft 
+
+--test_bft4 :: Test
+--test_bft4 = bft 
+
+-- Funcao reachable
+test_reachable :: Test
+test_reachable = reachable g6 1 ~?= fromList[1,2,3,4]
+
+-- Funcao isPathOf
+test_isPathOf1 :: Test
+test_isPathOf1 = isPathOf [Edge 1 5,Edge 5 2,Edge 2 4] g6 ~?= False
+
+test_isPathOf2 :: Test
+test_isPathOf2 = isPathOf [Edge 1 3] g5 ~?= False
+
+test_isPathOf3 :: Test
+test_isPathOf3 = isPathOf [Edge 1 2, Edge 2 3] g4 ~?= True 
+
+-- Funcao path 
+
+
+
+main = runTestTT $ TestList [test_adj,test_swap,test_empty,test_isEmpty1,
+                             test_isEmpty2,test_isValid1,test_isValid2,
+                             test_isDAG1,test_isDAG2,test_isDAG3,test_isForest1,
+                             test_isForest2,test_isSubgraphOf1,test_isSubgraphOf2,
+                             test_transpose,test_union,test_bft1,test_bft2,test_reachable,
+                             test_isPathOf1,test_isPathOf2,test_isPathOf3]
 
 --
 -- Teste aleatório
@@ -105,7 +243,7 @@ prop_forest = forAll (forest :: Gen (Forest Int)) $ \g -> collect (length (edges
 --
 -- Defina propriedades QuickCheck para testar todas as funções
 -- do módulo Graph.
---
+-- 
 
 -- Exemplo de uma propriedade QuickCheck para testar a função adj          
 prop_adj :: Graph Int -> Property
